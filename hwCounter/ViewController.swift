affeb7c80b0e2951aecd97a3c8cbd4fe2d03b8d4
Anthony <AvdeyAV@gmail.com>
2023-02-13 17:30:20 +0600

    add counter label

--- hwCounter/ViewController.swift
+++ hwCounter/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
+    var counterLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubviews()
@@ -17,8 +19,25 @@ final class ViewController: UIViewController {
     private func setupSubviews() {
         view.backgroundColor = .systemGray4
         title = "Counter"
+        
+        createLabel()
     }
+    
+//MARK: label
+    private func createLabel() {
+        
+        counterLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 300))
+        counterLabel.center = view.center
+        counterLabel.text = "0"
+        counterLabel.textAlignment = .center
+        counterLabel.font = UIFont.systemFont(ofSize: 16 * 7)
 
-
+        view.addSubview(counterLabel)
+    }
+    
+    
+    
+    
+    
 }
 
