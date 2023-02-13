3c84e9648999b0ae457234d2acbbd6f638edc58c
Anthony <AvdeyAV@gmail.com>
2023-02-13 17:39:36 +0600

    add counterButton + methods hold and touch

--- hwCounter/ViewController.swift
+++ hwCounter/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class ViewController: UIViewController {
     
     var counterLabel = UILabel()
+    var counterButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,7 @@ final class ViewController: UIViewController {
         title = "Counter"
         
         createLabel()
+        createButton()
     }
     
 //MARK: label
@@ -35,7 +37,36 @@ final class ViewController: UIViewController {
         view.addSubview(counterLabel)
     }
     
+
+//MARK: button
+    func createButton() {
+        counterButton = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 16 * 2))
+        counterButton.center.x = view.center.x
+        counterButton.center.y = view.center.y + 100
+//        counterButton.backgroundColor = .orange
+        counterButton.setTitle("Tap or Hold", for: .normal)
+        counterButton.setTitleColor(.systemBlue, for: .normal)
+        counterButton.addTarget(self, action: #selector(holdButton), for: .touchDown )
+        counterButton.addTarget(self, action: #selector(touchButton), for: .touchUpInside )
+
+        view.addSubview(counterButton)
+    }
     
+//MARK - methods
+    @objc func touchButton() {
+//        counterValue += 1
+//        counterLabel.text = String(counterValue)
+//        holdTimer.invalidate()
+//        print("touch Button")
+    }
+    
+    @objc func holdButton() {
+//        counterValue +=  1
+//        counterLabel.text = String(counterValue)
+//        createTimer()
+        print("hold Button")
+    }
+
     
     
     
